44be923525c6dae96ec4c7126ed6feede159c30d
Abu Azis <muhammadazis801@gmail.com>
2020-11-27 06:11:05 +0700

    determining editing completion

--- food-app/src/components/SearchBar.js
+++ food-app/src/components/SearchBar.js
@@ -2,15 +2,18 @@ import React from "react";
 import { StyleSheet, TextInput, View } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-const SearchBar = ({ term, onTermChange }) => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" size={30} style={styles.iconStyle} />
       <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
         style={styles.inputStyle}
         placeholder="Search"
         value={term}
-        onChangeText={(newTerm) => onTermChange(newTerm)}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
       />
     </View>
   );
